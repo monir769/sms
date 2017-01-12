348549eac3a6319cac2b8804a5f99796dcb9b416
monir769 <monirjohnrakib@gmail.com>
2017-01-12 16:30:50 +0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Login Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+<div class="container">
+	<form>
+    	<h2 class="text-center">Login Page</h2>
+        	
+            <input type="text" class="form-control" name="User id" placeholder="User id">
+            <input type="password" class="form-control" name="Password" placeholder="Password">
+                	
+             <div class="align-center">
+             <button type="submit" class="btn btn-primary btn-block" name="Submit" value="Login">Login</button>
+             </div>
+     </form>
+</div>
+       
+
+</body>
+</html>
